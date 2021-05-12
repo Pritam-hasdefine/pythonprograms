2d2a769f5cd5c86959642ea566ce3723da9ddd72
Pritam-hasdefine <pritam9604@gmail.com>
2021-05-12 15:51:16 +0530

    burger

--- hungry.py
+++ hungry.py
@@ -3,5 +3,6 @@ hungry=input("Are you Hungry?")
 if hungry == "yes":
     print("take your lunch")
     print("take pizza")
+    print("I want to eat pizza")
 else:
     print("do your task completed")
